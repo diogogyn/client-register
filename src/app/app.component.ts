cc7d141c4ef352023a986822b753f2ffd5e7ebce
Diogo O Santos <diogogyn100@gmail.com>
2024-03-01 22:52:50 -0300

    tentando uma nova estrutura

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ClienteService } from './cliente.service';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  clienteForm: FormGroup;
-  clientes: any[] | undefined;
-
-  constructor(private fb: FormBuilder, private clienteService: ClienteService) {
-    this.clienteForm = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(10)]],
-      cpf: ['', Validators.required],
-      address: this.fb.group({
-        address: ['', Validators.required],
-        neighborhood: ['', Validators.required]
-      }),
-      phones: this.fb.array([])
-    });
-  }
-
-  ngOnInit() {
-    this.listarClientes();
-  }
-
-  adicionarTelefone() {
-    this.phones.push(this.fb.control('', Validators.required));
-  }
-
-  removerTelefone(index: number) {
-    this.phones.removeAt(index);
-  }
-
-  get phones() {
-    return this.clienteForm.get('phones') as any;
-  }
-
-  adicionarCliente() {
-    if (this.clienteForm.valid) {
-      this.clienteService.adicionarCliente(this.clienteForm.value).subscribe(response => {
-        console.log('Cliente adicionado com sucesso:', response);
-        this.listarClientes();
-        this.clienteForm.reset();
-      });
-    }
-  }
-
-  listarClientes() {
-    this.clienteService.listarClientes().subscribe(response => {
-      this.clientes = response.content;
-    });
-  }
+export class AppComponent {
+  title = 'client-register';
 }
-
